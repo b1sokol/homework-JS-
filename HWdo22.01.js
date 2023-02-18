ab0c48c78869b71a2dbc68d67d59a92c9b3a8583
b1sokol <113973047+b1sokol@users.noreply.github.com>
2023-02-18 18:37:09 +0200

    first commit

---
+++ HWdo22.01.js
@@ -0,0 +1,25 @@
+//1
+const a = 10;
+const b = 15; {
+return 10 < 15 ? 15 : 15;
+}
+//2
+const year = +prompt("Введіть ваш рік народження");
+console.log(2023 - year);
+//3
+const name1 = "Igor";
+const name2 = "Oleg";
+const name3 = "Olena";
+const name4 = "Nastya";
+switch (name) {
+    case "Igor":
+    case "Oleg":
+        console.log(M);
+    break;
+    case "Olena":
+    case "Nastya":
+        console.log(W);
+    break;
+    default:
+        console.log("ERROR")  ;          
+}
---
+++ HWdo29.01.js
@@ -0,0 +1,16 @@
+//1
+const user = {
+	name: true,
+	age: 12,
+  country: "Ukraine"
+}
+console.log(typeof true)
+//2
+const user1 = {
+    n: 1,
+    m: 2,
+    obj: {a: 1, 
+        b: 2, 
+        c: 3}
+}
+console.log(Object.keys(user1).length)
